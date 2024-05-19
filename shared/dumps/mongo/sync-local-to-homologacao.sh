c77b31c448e79ec94669f54b16574efd89271eaa
David Faria <davidfaria89@gmail.com>
2024-05-19 15:41:41 -0300

    chore: add hml, local and prod

--- shared/dumps/mongo/sync-local-to-homologacao.sh
+++ shared/dumps/mongo/sync-local-to-homologacao.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-rsync -ahvz -e "ssh -i ~/projects/plox_brasil/ssh/plox" /home/david/projects/plox_brasil/docker/shared/dumps/mongo/backup_mongo_2023-11-25_03-30-01.tar.bz2 plox@10.10.61.20:/home/plox/projects/plox_brasil/docker/shared/dumps/mongo/
+rsync -ahvz -e "ssh -i ~/projects/plox_brasil/ssh/plox" /home/david/projects/plox_brasil/docker/shared/dumps/mongo/backup_mongo_2024-05-18_18-46-57.tar.bz2 plox@10.10.61.20:/home/plox/projetos/plox_brasil/docker/shared/dumps/mongo/
 
-
+# /home/plox/projetos/plox_brasil/docker/shared/dumps/mongo
 # DESCOMPACTAR
 #tar -xvf backup_mongo_2023-11-25_03-30-01.tar.bz2 -C /caminho/do/seu/diretorio
